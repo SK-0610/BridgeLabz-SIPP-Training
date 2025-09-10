94c4dae5c9d8edd86c26de8807bccb320177b026
SK-0610 <shreyash60321@gmail.com>
2025-09-10 22:10:51 +0530

    [Shreyash kamthan].Add Problem For Java Fundamentals Day-2

---
+++ Java Fundamental/Question_10.java
@@ -0,0 +1,7 @@
+public class Question_10 {
+    public static void main(String[] args) {
+        double distanceInKm = 10.8;
+        double distanceInMiles = distanceInKm * 1.609;
+        System.out.println("The distance " + distanceInKm + " km in miles is " + distanceInMiles);
+    }
+}
